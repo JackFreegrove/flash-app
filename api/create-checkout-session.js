425aa5bd67e856e818ace44fcc33a81484359010
JackFreegrove <jackfreegrove@gmail.com>
2026-04-01 19:13:47 +0100

    Fix checkout function

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,9 +1,11 @@
+import Stripe from 'stripe';
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   const { priceId, mode, tier } = req.body;
 
   try {
